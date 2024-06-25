00ea31ab20bc344195c85edceb7331cf57a59091
Abhijit Sahoo <shawabhijit370@gmail.com>
2024-06-25 08:36:13 +0530

    creating react app and asential things

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import Header from './components/Header'
+
+
+function App() {
+  
+
+  return (
+    <>
+      <div className='pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden'>
+        <Header />
+      </div>
+    </>
+  )
+}
+
+export default App
